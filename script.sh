2b5df52c7561cd3f94d4487ff4faa51e359aa17d
HACKERAP <69503838+HACKERAP@users.noreply.github.com>
2020-08-11 16:40:29 +0530

    script.sh
    pkg update 
    pkg upgrade 
    pkg install git
    pkg install bash
    git clone https://github.com/HACKERAP/FACTOR.git
    cd FACTOR
    ls
    chmode +x script.sh
    ls
    ./script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This is a clander,date  and time and factor tool.
+clear
+printf "\e[1;30m █████╗        ██████╗     ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗\e[0m\n"
+printf "\e[1;31m██╔══██╗       ██╔══██╗    ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗\e[0m\n"
+printf "\e[1;32m███████║       ██████╔╝    ███████║███████║██║     █████╔╝ █████╗  ██████╔╝\e[0m\n"
+printf "\e[1;33m██╔══██║       ██╔═══╝     ██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗\e[0m\n"
+printf "\e[1;34m██║  ██║██╗    ██║         ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║\e[0m\n"
+printf "\e[1;35m╚═╝  ╚═╝╚═╝    ╚═╝         ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\e[0m\n"
+
+Welcome
+
+cal
+date
+printf "\e[1;44m Welcom to tool \e[0m\n"
+sleep 5
+pkg install coreutils
+clear
+printf "\e[1;42m WELCOME \e[0m\n"
+printf "\e[1;41m Enter the number of which you like to do factor: \e[0m\n"
+
+factor
